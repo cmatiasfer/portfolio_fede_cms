f164db65254d3fab3e7b3d953613e86dc3d186ba
mati-remo <32935356+mati-remo@users.noreply.github.com>
2020-11-13 12:54:01 -0300

    Update styles.css

--- src/Zard/FrontBundle/Controller/ExceptionController.php
+++ src/Zard/FrontBundle/Controller/ExceptionController.php
@@ -9,13 +9,6 @@ class ExceptionController extends AbstractController
 {
     public function showException()
     {
-        $lang = 'en';
-        $error404 = true;
-        // $image = $imagesWebRepository->findOneBy(["visible" => true , "section" => "404"],[], 1);
-        return $this->render('@front_views/404/error404.html.twig', [
-            'lang_url' => $lang,
-            'error404' => $error404,
-            'image' => $image
-        ]);
+        return $this->redirectToRoute('home');
     }
 }
